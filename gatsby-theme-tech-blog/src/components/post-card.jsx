8758acf0799cca5f5ce5845b4ae7d50d1f3c6b24
urielhdz <uriel.hdzc@gmail.com>
2019-10-10 10:57:24 -0500

    Reorganizaed the post-card.jsx component to avoid having links inaccesible

--- gatsby-theme-tech-blog/src/components/post-card.jsx
+++ gatsby-theme-tech-blog/src/components/post-card.jsx
@@ -7,27 +7,27 @@ import Tags from './tags';
 
 export default ({post})=>{
   return(
-    <Link to={post.slug}>
+    
+    <div sx={{
+      variant: 'blocks.card',
+      mt:12
+    }}>
       <div sx={{
-        variant: 'blocks.card',
-        mt:12
-      }}>
-        <div sx={{
-          variant: 'blocks.dark',
-          minHeight: '200px'
-        }} style={{display:'flex', alignItems:'flex-end'}}>
-          <Styled.h3 sx={{ color: 'primaryDark', margin: 0 }} style={{ textDecoration: 'none'}}>
-            {post.title}
-          </Styled.h3>
-          
-        </div>
-        <div sx={{p:12, variant:"blocks.flexContainer"}} >
-          <div style={{flex:"1"}}><Author author={post.author} /></div>
-          <div style={{ flex: 1 }}><Tags tags={post.tags} /></div>
-          
-        </div>
+        variant: 'blocks.dark',
+        minHeight: '200px'
+      }} style={{display:'flex', alignItems:'flex-end'}}>
+        <Styled.h3 sx={{ color: 'primaryDark', margin: 0 }} style={{ textDecoration: 'none'}}>
+          <Link to={post.slug}>{post.title}</Link>
+        </Styled.h3>
+        
       </div>
-    </Link>
+      <div sx={{p:12, variant:"blocks.flexContainer"}} >
+        <div style={{flex:"1"}}><Author author={post.author} /></div>
+        <div style={{ flex: 1 }}><Tags tags={post.tags} /></div>
+        
+      </div>
+    </div>
+    
   );
 }
 
